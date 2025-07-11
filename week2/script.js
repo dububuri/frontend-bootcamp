b8ce7f767521df83bae98a693cb8331537b761b4
dububuri <sod083210@naver.com>
2025-07-11 13:31:41 +0900

    week1

---
+++ week2/script.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector("button");
+function click() {
+  console.log("clicked")
+}
+btn.onclick = click;
+
+let func = function(arg1, arg2, ...argN) {
+   return expression;
+ };
+
+// arrow function
+ let func = (arg1, arg2, ...argN) => expression
+
+
+let obj = {
+   cat : "meow",
+   dog : "woof"
+};
+// Dot notation
+console.log(obj.cat);
+console.log(obj.dog);
+
+let obj = {
+   cat : "meow",
+   dog : "woof"
+};
+// Bracket Notation
+let cat = "cat";
+let dog = "dog";
+console.log(obj[cat]);
+console.log(obj[dog]);
